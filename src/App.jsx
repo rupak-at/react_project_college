68b240702a9fd452bb0f401114563dcd00f5294d
rupak-at <rupakb802@gmail.com>
2026-09-23 21:13:37 +0545

    Style components with clean card-based design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
 import Navbar from './components/Navbar'
 import useCart from './hooks/useCart'
 import CatalogPage from './pages/CatalogPage'
